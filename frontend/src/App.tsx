b8e3c70a6f2718d46b0ab6f014a2187fe29a0da3
Andrew Li <andrewli200504@gmail.com>
2025-04-05 17:23:56 -0700

    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState('');
+  const [inputValue2, setInputValue2] = useState('');
+  const [inputValue3, setInputValue3] = useState('');
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const handleChange2 = (event2) => {
+    setInputValue2(event2.target.value);
+  }
+  const handleChange3 = (event3) => {
+    setInputValue3(event3.target.value);
+  }
 
   return (
     <>
@@ -18,12 +29,36 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <form>
+      <label htmlFor="my-text-input">Enter food:</label>
+      <input
+        type="text"
+        id="my-text-input"
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <p>You entered: {inputValue}</p>
+    </form>
+    <form>
+      <label htmlFor="my-text-input">Enter expiration date:</label>
+      <input
+        type="text"
+        id="my-text-input"
+        value={inputValue2}
+        onChange={handleChange2}
+      />
+      <p>You entered: {inputValue2}</p>
+    </form>
+    <form>
+      <label htmlFor="my-text-input">Enter id:</label>
+      <input
+        type="text"
+        id="my-text-input"
+        value={inputValue3}
+        onChange={handleChange3}
+      />
+      <p>You entered: {inputValue3}</p>
+    </form>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
